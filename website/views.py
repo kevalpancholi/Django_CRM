23aea5c95456fdb0510835a96936122f5b824d24
kevalpancholi <keval.m.pancholi@gmail.com>
2024-12-03 16:58:10 +0000

    Added delete button with functionality

--- website/views.py
+++ website/views.py
@@ -73,3 +73,13 @@ def day_record(request, pk):
     else:
         messages.success(request, "You must login to view that page!")
         return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You must login to delete that record!")
+        return redirect('home')
